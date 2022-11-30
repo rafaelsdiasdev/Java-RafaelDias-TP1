116c91dfd89b31f274aa8c5ad08d1db3410c6d45
Rafael Dias <rsdias84@gmail.com>
2022-11-30 19:05:24 -0300

    delete

--- RafaelDias-TP1/src/NomeIncompletoException.java
+++
@@ -1,8 +0,0 @@
-public class NomeIncompletoException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-	public NomeIncompletoException(String mensagem) {
-		super(mensagem);
-	}
-}
