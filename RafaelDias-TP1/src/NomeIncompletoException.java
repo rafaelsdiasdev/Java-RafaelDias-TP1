93751327f377b15fd871223b73218077e4a282a8
Rafael Dias <rsdias84@gmail.com>
2022-11-30 18:49:40 -0300

    TP3 Fundamentos Desenvolvimento Java

---
+++ RafaelDias-TP1/src/NomeIncompletoException.java
@@ -0,0 +1,8 @@
+public class NomeIncompletoException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NomeIncompletoException(String mensagem) {
+		super(mensagem);
+	}
+}
